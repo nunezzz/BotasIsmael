662b4759218ca2d83d0a87c99ababf814896b1d8
nunezzz <130615877+nunezzz@users.noreply.github.com>
2025-04-12 20:37:07 -0400

    Add files via upload

---
+++ dist/main.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@nestjs/core");
+const app_module_1 = require("./app.module");
+const cors = require("cors");
+async function bootstrap() {
+    const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    app.use(cors({
+        origin: 'http://localhost:3001',
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        credentials: true,
+    }));
+    await app.listen(3000);
+    console.log("Application is running on: ${await app.getUrl()}");
+}
+bootstrap();
+//# sourceMappingURL=main.js.map
